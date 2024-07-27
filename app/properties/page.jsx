57b2a52c6ca6398a448d30f942ae0d7733fb3f6d
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2024-07-27 10:18:27 +0300

    create property card

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,14 +1,25 @@
-import Link from "next/link";
 import React from "react";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
 
 const PropertiesPage = () => {
-  console.log("server component");
   return (
-    <div>
-      <h1 className="text-3xl">Properties</h1>
-      <Link href="/">Go home</Link>
-      <Link href="/properties/123">Go Prperty</Link>
-    </div>
+    <section class="px-4 py-6">
+      <div class="container-xl lg:container m-auto">
+        <h2 class="text-3xl font-bold text-blue-500 mb-6 text-center">
+          Recent Properties
+        </h2>
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
 
