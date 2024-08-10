ce302b3a62f7357b98bbd0f5871cf44efc001a7b
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2024-08-10 23:03:17 +0300

    featured properties

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,14 @@ import Hero from "@/components/Hero";
 import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
 import React from "react";
+import FeaturedProperties from "@/components/FeaturedProperties";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <FeaturedProperties />
       <HomeProperties />
     </>
   );
